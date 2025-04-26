e409c109ff609d00aa60a5986939bcca39437620
ausatov <andrey.usatov@whitesharx.com>
2025-04-25 22:16:22 -0700

    remove vercel analytics

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,7 +2,6 @@ import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
 import { GlobalStyle } from './styles/global'
-import { Analytics } from "@vercel/analytics/react"
 
 import 'react-toastify/dist/ReactToastify.css'
 function Home() {
@@ -10,7 +9,6 @@ function Home() {
         <>
             <Header></Header>
             <Main></Main>
-            <Analytics />
             <Footer></Footer>
         </>
     )
